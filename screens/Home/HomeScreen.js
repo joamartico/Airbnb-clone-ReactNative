2dc5c9ff4b1e822fc14d8576aafbf5bef59873d2
Joaquín Marticorena <Joa@Rubens-MBP.fibertel.com.ar>
2020-12-27 19:08:21 -0300

    HomeScreen

---
+++ screens/Home/HomeScreen.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ImageBackground, Pressable, Text, View } from "react-native";
+import styles from "./styles";
+import {Fontisto} from "@expo/vector-icons"
+
+
+const HomeScreen = () => {
+	return (
+		<View>
+			 <Pressable style={styles.searchButton}>
+					<Fontisto name="search" color="#f15454"/>
+                    <Text style={styles.searchButtonText}>Explore nearby stays</Text>
+                </Pressable>
+			<ImageBackground
+				style={styles.image}
+				source={require("../../assets/images/background.jpg")}
+			>
+				<Text style={styles.title}> Go</Text>
+				<Text style={styles.title}> Near</Text>
+
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>Explore nearby stays</Text>
+                </Pressable>
+			</ImageBackground>
+		</View>
+	);
+};
+
+export default HomeScreen;
---
+++ screens/Home/styles.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    image: {
+        height: 500,
+        width: "100vw",
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+    title: {
+        fontSize: 80,
+        fontWeight: "bold",
+        color: "#fff",
+        marginLeft: 10,
+        marginTop: -15,
+    },
+    button: {
+        backgroundColor: "white",
+        width: 200,
+        height: 40, 
+        marginLeft: 25,
+        marginTop : 25,
+        justifyContent: 'center', alignItems: 'center',
+        borderRadius: 10
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    searchButton: {
+        backgroundColor: "white",
+        flexDirection: "row",
+        height: 60, 
+        justifyContent: 'center', alignItems: 'center',
+        position: "absolute",
+        top: 25,
+        left: 25,
+        right: 25,
+        zIndex: 100,
+        borderRadius: 50
+    },
+    searchButtonText: {
+        fontSize: 16,
+        fontWeight: "bold"
+    }
+})
+
+export default styles
